5c7b965b864b9210de1a63722fffd639ae5365be
marianasena1002-beep <marianasena1002@gmail.com>
2026-08-25 17:25:01 -0500

    Update DemoApplicationTests.java

--- demo/src/test/java/com/venta_celulares/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/venta_celulares/demo/DemoApplicationTests.java
@@ -1,12 +1,13 @@
 package com.venta_celulares.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class DemoApplication {
+@SpringBootTest
+class DemoApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @Test
+    void contextLoads() {
+        System.out.println("Prueba ejecutada correctamente");
     }
 }
